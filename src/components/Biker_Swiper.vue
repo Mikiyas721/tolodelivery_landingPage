1aa2e4dff780126a6c29599d49b8806a6d731dae
Mikiyas721 <Mikiyas721@gmail.com>
2020-02-05 11:56:42 -0600

    Added fonts and replaced biker swiper

--- src/components/Biker_Swiper.vue
+++ src/components/Biker_Swiper.vue
@@ -1,41 +1,16 @@
 <template>
     <v-app>
         <container id="container">
-
             <div id="title">
-                <h1 class="ml-12">Take a closer look</h1>
-                <h3>See how easily you can use our biker application</h3>
+                <h1 class="ml-12 merriweather">Take a closer look</h1>
+                <h3 class="josefinsans">See how easily you can use our customer application</h3>
             </div>
-            <!--v-for work on slide?-->
-            <carousel>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/1.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/2.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/3.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/4.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/5.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/6.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/7.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/8.jpg')" class="image"></v-img>
-                </slide>
-                <slide class="image_Slide">
-                    <v-img :src="require('../assets/biker_swiper/9.jpg')" class="image"></v-img>
-                </slide>
-            </carousel>
+            <v-carousel cycle="true" interval="2000" progress="true" show-arrows-on-hover="true" vertical="true">
+                <v-carousel-item
+                        v-for="(item,i) in image_url" :src="item.path" :key="i"
+                        style="width: 240px; height: auto; margin-left: 42%">
+                </v-carousel-item>
+            </v-carousel>
         </container>
 
     </v-app>
@@ -71,13 +46,4 @@
         margin-bottom: 50px;
     }
 
-    .image {
-        height: 500px;
-        margin-left: 50%;
-    }
-
-    .image_Slide {
-        margin:20px 0px 0px 13%;
-
-    }
 </style>
